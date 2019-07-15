928729a02de628805912c191b3ac09804601776b
Junaid <1094837@students.wits.ac.za>
2019-07-15 15:15:02 +0200

    add basic LCD to simple approach

--- SMA/SMA/SMA.ino
+++ SMA/SMA/SMA.ino
@@ -1,7 +1,8 @@
 #include <TimeLib.h>
-
+#include <LiquidCrystal.h>
 unsigned long previousMillis = 0;        // will store last time ADC queried
 int ADCpin = A0;
+byte flag =0;
 
 const byte numStamps = 5; //n
 const byte averagePeriod = 5; //t
@@ -9,8 +10,11 @@ const byte averagePeriod = 5; //t
 // constants won't change:
 const int sampleRate = 120;
 const int slowestPulse = 3;
-const long interval = 1/sampleRate;           // interval at which to blink (milliseconds)
+const long interval = (1/(float)sampleRate) *1000;           // interval at which to blink (milliseconds)
 const int windowSize=sampleRate*slowestPulse;
+
+const byte dispInterval = 1000;           // interval at which to blink (milliseconds)
+unsigned long dispMillis = 0;        
 
 
 float sampleWindow[windowSize]={0};
@@ -18,9 +22,13 @@ float RR=0;
 float SMA=0;
 time_t stamps[numStamps];
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  lcd.begin(16, 2);
 }
 
 float calcSMA()
@@ -45,14 +53,21 @@ float appendWindow(float val)
 
 void detect(float val)
 {
-  if(val>SMA)
+  if(val>SMA && flag==0)
   {
+    flag=1;
     for (int i=0;i<numStamps-1;i++)
     {
       stamps[i]=stamps[i+1];
     }
     stamps[numStamps-1] = now();
   }
+  
+  if(val<SMA && flag==1)
+  {
+    flag=0;
+  }
+  
 }
 void updateRR()
 {
@@ -79,8 +94,21 @@ void loop() {
     float voltage = sensorValue * (5.0 / 1023.0);
     appendWindow(voltage);
     calcSMA();
-    
+
     Serial.println(SMA);
+  }
+   if (currentMillis - dispMillis >= dispInterval) {
+    dispMillis = currentMillis;
+    lcd.setCursor(0, 0);
+    lcd.print(F("RR: "));
+    lcd.print(RR,5);
+
+    lcd.setCursor(0, 1);
+    lcd.print(F("SMA: "));
+    lcd.print(SMA,5);
+    lcd.print(F("S:"));
+    lcd.print(interval);
+
   }
   // put your main code here, to run repeatedly:
 
