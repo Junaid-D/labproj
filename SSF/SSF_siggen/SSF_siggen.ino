d6b9827e002c441c65d19ed3dcb67b2731c32832
Junaid <1094837@students.wits.ac.za>
2019-07-16 10:38:18 +0200

    Add basic SSF with gradient approach

--- SSF/SSF_siggen/SSF_siggen.ino
+++ SSF/SSF_siggen/SSF_siggen.ino
@@ -1,37 +1,36 @@
 #include <TimeLib.h>
-#include <math.h>
 #include <LiquidCrystal.h>
 unsigned long previousMillis = 0;        // will store last time ADC queried
 int ADCpin = A0;
 byte flag =0;
 
-const byte numStamps = 6; //n
-const byte averagePeriod = 10; //t
+const byte numStamps = 5; //n
+const byte averagePeriod = 5; //t
 
 // constants won't change:
-const byte sampleRate = 120;
-const byte scaledSampleRate = 30;
-const byte slowestPulse = 6;
-byte decimCtr = 0;
-
-const byte scaleFac = sampleRate/scaledSampleRate;
-
+const int sampleRate = 120;
+const float slowRate = 0.2;
 const long interval = (1/(float)sampleRate) *1000;           // interval at which to blink (milliseconds)
-const int windowSize=scaledSampleRate*slowestPulse;
 
+
+
+
+const int windowSize=(sampleRate/slowRate)/32;
+float sampleWindow[windowSize]={0}; // holds SSF signal
 
 
 const byte dispInterval = 1000;           // interval at which to blink (milliseconds)
 unsigned long dispMillis = 0;        
 
+const int SSFSize = (sampleRate/slowRate)/2;
+float SSFWindow[SSFSize] ={0} ;
 
-float sampleWindow[windowSize] = {0};
+byte SSFfull = 0;
 
-float decimWindow[scaleFac] = {0};
 
 float RR=0;
-float SMA=0;
-int stamps[numStamps];
+float Grad = 0;
+unsigned long stamps[numStamps];
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -42,38 +41,8 @@ void setup() {
   lcd.begin(16, 2);
 }
 
-void fillDecim(float val)
-{
-    for (int i=0;i<scaleFac-1;i++)
-    {
-      decimWindow[i]=decimWindow[i+1];
-    }
-    decimWindow[scaleFac-1] = val;
-  
-}
 
-float decimate()
-{
-  float temp=0;
-  for (int i=0;i<scaleFac;i++)
-    {
-      temp+=decimWindow[i];
-    }
-    return temp/scaleFac;
-}
-
-
-void calcSMA()
-{
-  float temp=0;
-  for (int i=0;i<windowSize;i++)
-  {
-    temp+=sampleWindow[i];
-  }
-  SMA=temp/windowSize;
-}
-
-float appendWindow(float val)
+void appendWindow(float val)
 {
   for (int i=0;i<windowSize-1;i++)
   {
@@ -83,9 +52,55 @@ float appendWindow(float val)
   
 }
 
-void detect(float val)
+void appendSSFWindow(float val)
 {
-  if(val>SMA && flag==0)
+  if(SSFfull == 0 && SSFWindow[0]>0)
+    SSFfull=1;
+  
+  for (int i=0;i<SSFSize-1;i++)
+  {
+    SSFWindow[i]=SSFWindow[i+1];
+  }
+  SSFWindow[SSFSize-1] = val;
+
+}
+
+void calcGrad()
+{
+  float xx = 0;
+  float xy = 0;
+  float x = 0;
+  float y = 0;
+  
+  for (int i=0;i<windowSize;i++)
+  {
+    x+=i;
+    y+=sampleWindow[i];
+    xx+= i*i;
+    xy+= sampleWindow[i]*i;
+  }
+
+  Grad = (windowSize*xy - x*y)/(windowSize*xx-x*x);
+
+}
+
+float getSSFVal()
+{
+  float zi=0;
+  for (int i=0;i<SSFSize-1;i++){
+    float yk = 0;
+    yk=SSFWindow[i+1]-SSFWindow[i];
+    if(yk>0)
+          zi=zi+yk;
+  }
+  Serial.println(zi);
+
+  return zi;
+}
+
+void detect()
+{ 
+  if(Grad<0 && flag==0)
   {
     flag=1;
     for (int i=0;i<numStamps-1;i++)
@@ -95,12 +110,13 @@ void detect(float val)
     stamps[numStamps-1] = millis();
   }
   
-  if(val<SMA && flag==1)
+  if(Grad>0 && flag==1)
   {
     flag=0;
   }
   
 }
+
 void updateRR()
 {
   int avg = 0;
@@ -116,7 +132,6 @@ void updateRR()
   avg/=(count);// in millisec
 
   float freq=avg/((float)1000);
-  Serial.println(1/freq);
   RR=(60)/freq; //convert to BPM
   
 }
@@ -125,21 +140,23 @@ void loop() {
   unsigned long currentMillis = millis();
   float t = currentMillis/((float)1000);
    if (currentMillis - previousMillis >= interval) {
-    decimCtr++;
     previousMillis = currentMillis;
-    //int sensorValue = analogRead(A0);
-    float freq = 2*M_PI*0.2;
-    float voltage = 3.2 +  0.2*sin (freq*t);
 
-    fillDecim(voltage);
-  }
-  
-  if(decimCtr==scaleFac){
-    appendWindow(decimate());
-    calcSMA();
-    detect(decimate());
-    updateRR();
-    decimCtr=0;
+    //int sensorValue = analogRead(A0);
+    
+    float freq = 2*M_PI*0.1;
+    float voltage = 3.2 +  0.2*sin (freq*t);
+    
+    appendSSFWindow(voltage);
+
+    if(SSFfull == 1)
+    {
+      appendWindow(getSSFVal());
+      calcGrad();
+      detect();
+      updateRR();
+    }
+    
   }
   
    if (currentMillis - dispMillis >= dispInterval) {
@@ -149,8 +166,8 @@ void loop() {
     lcd.print(RR,5);
 
     lcd.setCursor(0, 1);
-    lcd.print(F("SMA: "));
-    lcd.print(SMA,5);
+    lcd.print(F("Grad:"));
+    lcd.print(Grad,3);
     lcd.print(F("S:"));
     lcd.print(interval);
 
