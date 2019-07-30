e780cd6cd9640918c5cf8f17bb40f5fd71a0abc8
Junaid <1094837@students.wits.ac.za>
2019-07-30 13:22:11 +0200

    add first outside group test

--- Testing/Findpeaks_comparison.m
+++ Testing/Findpeaks_comparison.m
@@ -5,7 +5,7 @@ folder = 'Full_test';
 list = dir(folder)
 ctr = 0;
 Errs = 0;
-for i=1:10
+for i=1:length(list)
     if(list(i).isdir==1 || ~isempty(strfind(list(i).name,'speaker')))
         continue;
     end
@@ -21,13 +21,15 @@ for i=1:10
 
     [~,idu] = unique(detsWStamps(:,3))
     uniqueDetVals = detsWStamps(idu,:);
+    uniqueDetVals = uniqueDetVals(2:end,:);
+
 
     [pk,lk] = findpeaks(sig);
-    figure()
+    %figure()
     hold on;
-    plot(time,sig);
-    plot(uniqueDetVals(:,5).',uniqueDetVals(:,2).','*')
-    plot(time(lk),pk,'o')
+   % plot(time,sig);
+   % plot(uniqueDetVals(:,5).',uniqueDetVals(:,2).','*')
+   % plot(time(lk),pk,'o')
 
     hold off;
 
@@ -35,3 +37,22 @@ for i=1:10
 
 end
 Errs/ctr
+
+function out = getAttr(name)
+
+strs = strsplit(name)
+
+res.name = strs(1);
+res.ambient = strs(2);
+res.depth = strs(3);
+res.rate = strs(4);
+res.mask = strs(5);
+res.interrupted = strs(6);
+res.orifice = strs(7);
+
+
+
+
+return res;
+
+end
